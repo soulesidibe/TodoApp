e8fcc3a96b6c3b3b8a6e7f2888e322c1bbe4c0b2
Souleymane Sidibe <souleymane.sidibe.external@worldline.com>
2021-06-21 20:19:13 +0000

    delete fake data and and an inmemory database

--- app/src/main/java/com/soulesidibe/todoapp/ui/TodoViewModel.kt
+++ app/src/main/java/com/soulesidibe/todoapp/ui/TodoViewModel.kt
@@ -5,19 +5,24 @@ import androidx.lifecycle.ViewModel
 import com.soulesidibe.todocompose.data.Todo
 
 class TodoViewModel : ViewModel() {
+
+    private val todos: MutableList<Todo> = mutableListOf()
+
     fun get(id: String?): Todo? {
-        return id?.let {
-            Todo(title = "Test 1")
+        return id?.let { todoId ->
+            val first = todos.firstOrNull {
+                todoId == it.id
+            }
+
+            first
         }
     }
 
-    private val _todos: MutableLiveData<List<Todo>> = MutableLiveData(
-        listOf(
-            Todo(title = "Test 1"),
-            Todo(title = "Test 2"),
-            Todo(title = "Test 3"),
-            Todo(title = "Test 4"),
-        )
-    )
+    fun add(todo: Todo) {
+        todos.add(todo)
+        _todos.postValue(todos)
+    }
+
+    private val _todos: MutableLiveData<List<Todo>> = MutableLiveData(todos)
     val todosLiveData = _todos
 }

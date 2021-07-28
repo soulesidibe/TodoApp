e34d0c69fb63bfb32ec9ebd4e90a644487b14c46
Souleymane Sidibe <souleymane.sidibe.external@worldline.com>
2021-07-28 18:51:17 +0000

    update compose to 1.0.0 🚀 and also build tools 7.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
 //        kotlinCompilerVersion = "1.4.32"
         kotlinCompilerVersion = "1.5.10"
-        kotlinCompilerExtensionVersion = "1.0.0-beta08"
+        kotlinCompilerExtensionVersion = "1.0.0"
     }
 }
 
@@ -61,23 +61,23 @@ dependencies {
     // Koin main features for Android (Scope,ViewModel ...)
     implementation("io.insert-koin:koin-android:$koin_version")
 
-    implementation("androidx.core:core-ktx:1.5.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha08")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha03")
-    implementation("androidx.compose.material:material:1.0.0-beta09")
-    implementation("androidx.compose.ui:ui:1.0.0-beta09")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta09")
-    implementation("androidx.compose.compiler:compiler:1.0.0-beta09")
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta09")
-    implementation("androidx.compose.material:material-icons-core:1.0.0-beta09")
-    implementation("androidx.compose.material:material-icons-extended:1.0.0-beta09")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.12.0")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-beta01")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+    implementation("androidx.compose.material:material:1.0.0")
+    implementation("androidx.compose.ui:ui:1.0.0")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0")
+    implementation("androidx.compose.compiler:compiler:1.0.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
+    implementation("androidx.compose.material:material-icons-core:1.0.0")
+    implementation("androidx.compose.material:material-icons-extended:1.0.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.14.0")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-beta02")
+    implementation("androidx.activity:activity-compose:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")

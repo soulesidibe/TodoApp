05c9cac25753dd122bfab6c826a9103b60e2a362
Souleymane Sidibe <souleymane.sidibe.external@worldline.com>
2021-08-19 19:01:01 +0000

    delete useless deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,13 +72,11 @@ dependencies {
     implementation("androidx.compose.ui:ui:${compose_version}")
     implementation("androidx.compose.ui:ui-tooling:${compose_version}")
     implementation("androidx.compose.compiler:compiler:${compose_version}")
-//    implementation("androidx.compose.runtime:runtime-livedata:${compose_version}")
     implementation("androidx.compose.material:material-icons-core:${compose_version}")
     implementation("androidx.compose.material:material-icons-extended:${compose_version}")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.14.0")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha03")
-//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")

589af2698b3631d5e5a28ab2e78bc6877310fb41
Souleymane Sidibe <souleymane.sidibe01@gmail.com>
2022-03-25 23:31:41 +0000

    bump compile sdk version to 31

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val minSdk = 21
-    const val compileSdk = 30
+    const val compileSdk = 31
     const val compose = "1.0.0"
     const val room = "2.3.0"
     const val koin = "3.2.0-beta-1"
